f92ffd2b76e472dcbb614b5b250238a601b909e8
student <student@ws03-407.vvtis>
2024-01-12 10:39:04 -0100

    sum row

--- src/sum_row.cpp
+++ src/sum_row.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+#include <time.h>
+#include <math.h>
 
+#define _USE_MATH_DEFINES
 
+using namespace std;
 
+const long long int N = 1e9;
+
+int main() {
+ double sum = 0;
+ double start_time = clock();
+ for (long long int i = 1; i<=N; i++) {
+  sum += (pow(i,(1.0/3)) / ((i + 1) * sqrt(i)));
+ }
+ double end_time = clock();
+ cout << "time = " << (end_time - start_time) / CLOCKS_PER_SEC<< endl;
+ cout << "sum = " << sum << endl;
+
+ return 0;
+}
