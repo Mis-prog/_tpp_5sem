5f413ae30589d3d1271bf99f6628bc255850fd89
student <student@WS03-407>
2024-02-12 17:40:54 +0500

    rectoring code

--- src/multiplication_matrix.cpp
+++ src/multiplication_matrix.cpp
@@ -1,53 +1,124 @@
-#include <stdlib.h>
-// #include <openacc.h>
-#include <time.h>
-#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <omp.h>
+#include "windows.h"
+#include "multiplication_matrix.h"
 
-const int N = 256;
-double A[N][N];
-double B[N][N];
-double C[N][N];
+#define N 100
 
+double FirstMatrix[N][N];
+double SecondMatrix[N][N];
+double Product[N][N];
 
 int main() {
-int size=N;
-	int q = 10;
-	srand(time(NULL));
-//scanf("%d",&q);
-	clock_t t1, t2;
-long long cgpu;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++) {
-			A[i][j] = (rand() % 256) / 256.0 - 0.5;
-			B[i][j] = (rand() % 256) / 256.0 - 0.5;
-		}
+	SetConsoleOutputCP(CP_UTF8);
 
-	////////////////////////////////////
-	t1=clock();
-// #pragma acc enter data copyin(A[0:size][0:size],B[0:size][0:size]) 
-// #pragma acc enter data create(C[0:size][0:size])
-	for (int l = 0; l < q; l++) {
-	// #pragma acc parallel loop 
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < N; j++) {
-				C[i][j] = 0;
-				for (int k = 0; k < N; k++) C[i][j] += A[i][k] * B[k][j];
-			}
+    int q = 0;
+    std::cout << "Введите количество умножений: ";
+    std::cin >> q;
+    std::cout << std::endl;
+    srand(time(NULL));
+
+    set_rand_matrix();
+
+    double start_time = omp_get_wtime();
+    for (int p = 0; p < q; p++) {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                Product[i][j] = 0;
+                for (int k = 0; k < N; k++)
+                    Product[i][j] += FirstMatrix[i][k] * SecondMatrix[k][j];
+            }
+    }
+    double end_time = omp_get_wtime();
+    
+	double Euclid = 0.0;
+    norm(Euclid);
+	std::cout << "Вычисление нормы:\n";
+    print(Euclid, end_time, start_time);
+
+    start_time = omp_get_wtime();
+    for (int p = 0; p < q; p++) {
+	#pragma omp parallel for num_threads(4)
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                Product[i][j] = 0;
+                for (int k = 0; k < N; k++)
+                    Product[i][j] += FirstMatrix[i][k] * SecondMatrix[k][j];
+            }
+    }
+    end_time = omp_get_wtime();
+    
+	Euclid = 0.0;
+    norm(Euclid);
+    std::cout << "Первый алгоритм" << std::endl;
+    print(Euclid,end_time,start_time);
+
+
+    start_time = omp_get_wtime();
+    for (int l = 0; l < q; l++) {
+        for (int i = 0; i < N; i++){
+    #pragma omp parallel for num_threads(4)
+            for (int j = 0; j < N; j++) {
+                Product[i][j] = 0;
+               	for (int k = 0; k < N; k++) 
+                   	Product[i][j] += FirstMatrix[i][k] * SecondMatrix[k][j];
+              	}
+            }
 	}
-	t2=clock();
-	double s = 0.0;
-	// #pragma acc parallel loop 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++) s += C[i][j] * C[i][j];
-		
-		cgpu = t2-t1;
-	/////////////////////////////////////
-	// #pragma acc exit data copyout(C[0:size][0:size])
-//#pragma acc exit delete(A[0:size][0:size],B[0:size][0:size],C[0:size][0:size])	
-	
-	printf( "%13d iterations completed\n", q );
-	printf( "%13lld ms on GPU\n", cgpu );
+    end_time = omp_get_wtime();   
+    
+	Euclid = 0.0;
+    norm(Euclid);
+    std::cout << "Второй алгоритм" << std::endl;
+    print(Euclid,end_time,start_time);
 
+    start_time = omp_get_wtime();
+    for (int l = 0; l < q; l++)
+    {
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++)
+            {
+                Product[i][j] = 0;
+                double sum = 0.0;
+    #pragma omp parallel for reduction(+: sum) num_threads(4)
+                    for (int k = 0; k < N; k++){
+                        sum += FirstMatrix[i][k] * SecondMatrix[k][j];
+					}
+                    Product[i][j] = sum;
+    	    }
+		}
+    }
+    end_time = omp_get_wtime();
+
+    Euclid = 0.0;
+    norm(Euclid);
+    std::cout << "Третий алгоритм" << std::endl;
+    print(Euclid,end_time,start_time);
 
 	return 0;
+}
+void set_rand_matrix()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            FirstMatrix[i][j] = (rand() % 10) / 10.0 - 0.5;
+            SecondMatrix[i][j] = (rand() % 10) / 10.0 - 0.5;
+        }
+    }
+}
+void norm(double &Euclid)
+{
+// #pragma omp parallel for num_threads(4)
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            Euclid += Product[i][j] * Product[i][j];
+}
+void print(double Euclid, double end_time, double start_time)
+{
+    std::cout << "Норма:" << Euclid << std::endl;
+    std::cout << "Время: " << end_time - start_time << std::endl;
+    std::cout << std::endl;
 }
---
+++ src/multiplication_matrix.h
@@ -0,0 +1,6 @@
+#pragma once
+void print(double Euclid, double end_time, double start_time);
+
+void norm(double &Euclid);
+
+void set_rand_matrix();
